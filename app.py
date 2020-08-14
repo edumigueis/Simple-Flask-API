552fce8f1af9f6d59fc8f154d966d42661b23f72
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-08-14 11:44:14 -0300

    Adjustements

--- app.py
+++ app.py
@@ -63,12 +63,6 @@ table {
     border-collapse: collapse;
     border-spacing: 0px;
 }
-@media only screen and (max-width: 700px){
-    .background img{
-       object-fit: contain;
-       height: 100%;
-   }
-}
    </style>
 </head>
 
